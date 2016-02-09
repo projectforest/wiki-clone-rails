e8e8423c2a6ee478161c347055bdc72137758d6f
hlinforest <hllin@sfu.ca>
2016-02-09 14:30:22 -0800

    enhanced articles on index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show]
   def index
+    @articles = Article.all.order("created_at DESC")
   end
 
   def new
@@ -15,8 +17,16 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def show
+
+  end
+
   private
 
+  def find_article
+    @article = Article.find(params[:id])
+  end
+  
   def article_params
     params.require(:article).permit(:title, :content)
   end
